3e37d60bcb63920a554374c18a2523364d125321
adilet <adiletbeishekeyev@gmail.com>
2025-09-03 16:30:06 +0500

    Add Task model and initial migration for todo_list app
    - Add Task model with title, description, due_date, is_done, priority,
    and user - Add initial migration for Task model - Register todo_list app
    in settings - Set TIME_ZONE to Asia/Almaty - Add SQLite database file

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+    created_at = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    is_done = models.BooleanField(default=False)
+    priority = models.IntegerField(
+        choices=[(1, "Low"), (2, "Medium"), (3, "High")],
+        default=2,
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
